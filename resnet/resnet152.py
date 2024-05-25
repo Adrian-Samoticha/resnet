1daa118fb4256fe295105e32a1ce683a30659b6b
Adrian Samoticha <86920182+Adrian-Samoticha@users.noreply.github.com>
2024-05-25 16:04:06 +0200

    replace `keras.engine` import with `from keras import layers`

--- resnet/resnet152.py
+++ resnet/resnet152.py
@@ -21,7 +21,8 @@ from keras.layers import (
 from keras.layers import BatchNormalization
 from keras.models import Model
 from keras import initializers
-from keras.engine import Layer, InputSpec
+# from keras.engine import Layer, InputSpec
+from keras import layers
 from keras.engine.topology import get_source_inputs
 from keras import backend as K
 from keras.applications.imagenet_utils import _obtain_input_shape
